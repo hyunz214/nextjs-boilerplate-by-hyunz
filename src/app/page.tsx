ab5680a3fcae82eb6c129edf24548acbc6e12eb9
hyunz <jirae1997@naver.com>
2025-07-01 17:57:25 +0900

    💄 tailwind style 설정 방식 변경 작업 완료

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function HomePage() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <h1>home</h1>
-        <Button variant="secondary">버트은</Button>
+        <Button>버트은</Button>
       </main>
     </div>
   );
